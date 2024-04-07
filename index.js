e43f40cb9dc6ebe551b52efbb06e3d87ef7c160e
user.name <akamrannicole@gmail.com>
2024-04-07 12:15:42 +0300

    DOMcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+// Wait for the DOM content to load
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the element with the ID 'text'
+    const textElement = document.getElementById('text');
+    // Change its text content
+    textElement.textContent = 'This is really cool!';
+});
